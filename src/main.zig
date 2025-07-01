80e0330deee7baa89fd3791e2a7d954902ce58d4
MONISHSHARMA080 <monishsharma080@gmail.com>
2025-07-01 13:43:20 +0530

    implemented , could use more perfromance improvements, but we are good

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,26 @@
 const std = @import("std");
 const print = std.debug.print;
+const ThreadPool = std.Thread.Pool;
 
 pub fn main() !void {
     var srtTime = try std.time.Timer.start();
     const dictFile = @embedFile("./cs50/dictionaries/large");
-    const nanoseconds = srtTime.read();
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
+    const noOfWorkers: u32 = 9;
+
+    var threadPool: ThreadPool = undefined;
+    try threadPool.init(std.Thread.Pool.Options{ .allocator = allocator, .n_jobs = noOfWorkers });
+    defer threadPool.deinit();
+
     const dict = WordDictionary;
-    try dict.openFileAndReturnsHashTableOfAllTheWords("./cs50/dictionaries/large", allocator);
+    try dict.openFileAndReturnsHashTableOfAllTheWords("./cs50/dictionaries/large", allocator, &threadPool, noOfWorkers);
     defer dict.HashMap.deinit(allocator);
 
+    const nanoseconds = srtTime.read();
     const a: f64 = @floatFromInt(nanoseconds);
     const timeInMs: f64 = a / 1_000_000.0;
     std.debug.print("the file lenght is {d} and the frst [0..50]m content is ->{s} ------\n time passed since the start is {d:.10} ms \n ", .{ dictFile.len, dictFile[0..10], timeInMs });
@@ -21,48 +28,80 @@ pub fn main() !void {
 
 const WordDictionary = struct {
     const this = @This();
-    var HashMap = std.hash_map.HashMapUnmanaged([:0]const u8, bool, std.hash_map.StringContext, 92){};
+    var HashMap = std.hash_map.HashMapUnmanaged([:0]u8, bool, std.hash_map.StringContext, 92){};
 
-    pub fn openFileAndReturnsHashTableOfAllTheWords(comptime fileLocation: [:0]const u8, allocator: std.mem.Allocator) !void {
+    pub fn openFileAndReturnsHashTableOfAllTheWords(comptime fileLocation: [:0]const u8, allocator: std.mem.Allocator, threadPool: *ThreadPool, comptime noOfWorkers: u32) !void {
         const dictFile = @embedFile(fileLocation);
+        // @compileLog("the type of the file is {any} ", .{@typeInfo(@TypeOf(dictFile))});
         std.debug.print("the file lenght is {d} and the frst [0..30]m content is ->{s} +++ \n from the struct \n", .{ dictFile.len, dictFile[0..30] });
         try HashMap.ensureTotalCapacity(allocator, 198_000);
-        try putTheWordsInTheHashMap(dictFile, allocator); // const lenOfLargestWord = this.getTheLenOfLargestWordInDict(dictFile);
-        //
+        // make a function that will return the NulChar and
+        // try putTheWordsInTheHashMap(dictFile, allocator); // const lenOfLargestWord = this.getTheLenOfLargestWordInDict(dictFile);
+        const indicesToGoToForWork = this.getDistributedWorkLocation(dictFile, noOfWorkers);
+        print("the indices to go to the work is {any} \n", .{indicesToGoToForWork});
+        var prevVal: u32 = 0;
+        print("launching threadPool to put the words in the hashMap \n ", .{});
+        var wg: std.Thread.WaitGroup = std.Thread.WaitGroup{};
+        for (indicesToGoToForWork, 0..) |value, i| {
+            print("on index:{d} and  dictFile[prevVal..value] --> dictFile[{d}..{d}]  \n", .{ i, prevVal, value });
+            try threadPool.spawn(this.putTheWordsInTheHashMap, .{ dictFile[prevVal..value], allocator, &wg });
+            wg.start();
+            prevVal = value;
+        }
 
-        // since the file is too large and we can't use the for loop then let's switch to out old trusty heap
-        // allocator.
-        //
-        //game plan -> make a array  where each index is the size of the max word , for eg the last word is zymurgy and get's its ascii value - a so we get the number of the array
-        // const lastIndexForKeysArray = this.getHashTablesLastIndex(dictFile);
-        // something is wrong here as 128 should not be the
-
-        // const largestValOfHash = this.getTheHashOfLargestWord(dictFile);
-        // const hashTableSize = this.getHashTableSize(lenOfLargestWord);
-        // print(" the hashTableSize(threotical) is {d} and largestValueOfHash(based on word) is {d} ++ \n", .{ hashTableSize, largestValOfHash });
-        // std.debug.assert(hashTableSize == largestValOfHash);
-
-        // const buf: [res.largestValue]u8 = dictFile[res.largestValueAtIndex - res.largestValue .. res.largestValue];
-        // std.debug.print("well the largest word in the dictionary is of lenght {d} and the buff is {c}, -- and the hashTable size is {d}  \n ", .{ lenOfLargestWord, "--not there/implemented--", hashTableSize });
-
-        // get the words from the dict and store it as the key and  bool or u2 as vlaue get it out, if true or the capture group is successfull then the word is there
-        // else not
-
+        threadPool.waitAndWork(&wg);
+        print("the thread pool finished\n", .{});
     }
 
-    fn putTheWordsInTheHashMap(file: [:0]const u8, alloc: std.mem.Allocator) std.mem.Allocator.Error!void {
-        var word: [50:0]u8 = undefined;
+    /// returns the nearest ascii 10 value's index #noOfWorkers times
+    fn getDistributedWorkLocation(comptime f: [:0]const u8, comptime noOfWorkers: u32) [noOfWorkers]u32 {
+        const workRange: u32 = f.len / noOfWorkers;
+        // eg if the noOfWorkers = 5 I want a 5 values for each worker, eg 1,2,3,4,5 at index 4 or noOfWorkers - 1
+        var resArray: [noOfWorkers]u32 = [_]u32{0} ** noOfWorkers;
+        var resArrIndex: u16 = 0;
+        print("the lenght of the file is {d} \n", .{f.len});
+        var i: u32 = workRange;
+        while (i < f.len and resArrIndex < resArray.len) : (i -= 1) {
+            std.debug.assert(i <= f.len); // you can't tell us to go to the index after the file
+            std.debug.assert(resArrIndex < resArray.len); // eg. arr of len 5 has index range from 0->4
+            std.debug.assert(resArrIndex < noOfWorkers); // eg. arr of len 5 has index range from 0->4
+            print("at the filelocation:{d} \n ", .{i});
+
+            if (f[i] == 10) {
+                resArray[resArrIndex] = i;
+                resArrIndex += 1;
+                // got the value and now update the i to go to the next one
+                i = i + workRange;
+                print("\n got the ascii value 10 at index:{d} and new index in file is:{d} and the resultArrayIndex is {d} and the resultArray is {any} \n\n ", .{ i - workRange, i, resArrIndex, resArray });
+                // not asserting here as I am not accessing the file here and in the case of the last i(index) It can be as this is not a problem
+                // std.debug.assert(i <= f.len); // you can't tell us to go to the index after the file
+                continue;
+            }
+        }
+        return resArray;
+    }
+
+    /// get's the word from the file and add it to the hashMap -- if we get the ascii 10 we assume we have a new char and add the prev one in hashMap
+    /// if for some reason the last thing in the file is not 10 and we got some words then it will treat it as words and add it in the hashMap too !!
+    ///
+    ///NOTE:-> the file here is not :0(0/null terminated/sentinel ) as I am indexing the file, and the func assumes the file ends when it runds out of words and not when we get
+    /// ascii 0 value
+    //
+    fn putTheWordsInTheHashMap(file: []const u8, alloc: std.mem.Allocator, wg: *std.Thread.WaitGroup) void {
+        defer wg.finish();
+        var word: [59:0]u8 = undefined;
         var indexInWord: u32 = 0;
         for (file, 0..) |value, i| {
             // we can't over index on the word array
             std.debug.assert(indexInWord <= word.len);
             if (value == 10) {
                 // got to a line break
-                indexInWord = 0;
                 word[indexInWord] = 0; // ascii for the null termination
-                print("at index:{d} we are at a linebreak ->{c}<-or in ascii val:{d} and putting nullTerminator the word var and adding it to the hashMap \n\n", .{ i, value, value });
+                print("at index:{d} we are at a linebreak ->{c}<-or in ascii val:{d} and putting nullTerminator the word var that is ->{s}<- to the hashMap \n\n", .{ i, value, value, word[0..indexInWord] });
                 // put it in the hashMap
-                try this.HashMap.put(alloc, &word, true);
+                // try this.HashMap.put(alloc, &word, true);
+                this.HashMap.put(alloc, word[0..indexInWord :0], true) catch |e| std.debug.panic("\n the put call in the hashMap returned an error and we are not able to put stuff in the hashMap so we crash, error is ->{any} \n\n ", .{e});
+                indexInWord = 0;
                 continue;
             } else {
                 word[indexInWord] = value;
@@ -70,7 +109,15 @@ const WordDictionary = struct {
                 print("at index:{d} we've got ->{c}<-or in ascii val:{d} and putting this char at the indexInWord:{d} and the word[0..{d}] is {s} \n ", .{ i, value, value, indexInWord, indexInWord, word[0..indexInWord] });
             }
         }
-        // this.hashMap;
+        // if we are at the end of the file and not got the ascii 10 then we would skip the last word so (this func could be running on diff thread too) so of the ascii 10
+        // is not there then let's add it, we will assert (or in that case) indexInWord != 0 and word does not have 0 at the first place
+        if (indexInWord != 0) {
+            std.debug.assert(word[0] != 10);
+            // try this.HashMap.put(alloc, &word, true);
+            word[indexInWord] = 0; // in the else block above we have moved the value by one os null on this one
+            this.HashMap.put(alloc, word[0..indexInWord :0], true) catch |e| std.debug.panic("\n the put call in the hashMap returned an error and we are not able to put stuff in the hashMap(in the last case) so we crash, error is ->{any} \n\n ", .{e});
+            print("-- we were not able to find the end ascii 10 and the indexInWord != 0, it is {d} so adding a new word: {s}    \n", .{ indexInWord, word[0..indexInWord] });
+        }
     }
 
     /// goes through the dictFile and if there is a largest word then return it
